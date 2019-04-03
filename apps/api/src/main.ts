38947f236ef909bc9a4bd7c7aee9de6f2a4fb12c
mJehanno <mathob.jehanno@hotmail.fr>
2019-04-03 09:45:42 +0200

    chore(Configuration): Adds middleware config for Nest (Security-based middleware)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,25 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app/app.module";
 
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as compression from 'compression';
+
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api`);
+
+  /**
+   * * Middlewares
+   */
+  app.use(helmet());
+  app.use(csurf());
+  app.use(compression());
+  app.enableCors();
+  /**
+   * * Bootstrapping app
+   */
   const port = process.env.port || 3333;
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
